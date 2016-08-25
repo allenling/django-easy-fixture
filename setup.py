d9ab4b51df51c33570b6f9defcdcecd9cc54a3b3
allenling <allenling3@gmail.com>
2016-08-25 10:32:18 +0800

    clean

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
                     'Operating System :: OS Independent',
                     'Programming Language :: Python',
                     'Programming Language :: Python :: 2',
+                    'Programming Language :: Python :: 2.7',
       ],
       license='MIT',
       packages=packages,
